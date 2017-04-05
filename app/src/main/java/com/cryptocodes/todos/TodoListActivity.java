9202196ee030dd6412e483b3826b034681815259
CORP\Jonathan.Fraimorice <jonathan.fraimorice@clarizen.com>
2017-04-05 16:15:04 +0300

    Add read/write data to the DB

--- app/src/main/java/com/cryptocodes/todos/TodoListActivity.java
+++ app/src/main/java/com/cryptocodes/todos/TodoListActivity.java
@@ -1,12 +1,15 @@
 package com.cryptocodes.todos;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,9 +34,10 @@ public class TodoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHelper helper = new DatabaseHelper(this);
-        SQLiteDatabase db = helper.getReadableDatabase();
-
+    //    DatabaseHelper helper = new DatabaseHelper(this);
+      //  SQLiteDatabase db = helper.getReadableDatabase();
+     //   CreateTodo();
+        readData();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final ListView lv=(ListView) findViewById(R.id.lvTodos);
@@ -75,5 +79,30 @@ public class TodoListActivity extends AppCompatActivity {
                 + " VALUES (\"Go to the gym\", 1, \"2016-01-01\", \"\", 0)";
 
         db.execSQL(query);
+
+        ContentValues values = new ContentValues();
+        values.put(TodosContract.TodosEntry.COLUMN_TEXT, "Call Mr. Bean");
+        values.put(TodosContract.TodosEntry.COLUMN_CATEGORY, 1);
+        values.put(TodosContract.TodosEntry.COLUMN_CREATED, "2016-01-02");
+        values.put(TodosContract.TodosEntry.COLUMN_DONE, 0);
+        long todo_id = db.insert(TodosContract.TodosEntry.TABLE_NAME, null, values);
+    }
+
+    private void readData() {
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String[] projection = {TodosContract.TodosEntry.COLUMN_TEXT,
+                TodosContract.TodosEntry.COLUMN_CREATED,
+                TodosContract.TodosEntry.COLUMN_EXPIRED,
+                TodosContract.TodosEntry.COLUMN_DONE,
+                TodosContract.TodosEntry.COLUMN_CATEGORY};
+        String selection = TodosContract.TodosEntry.COLUMN_CATEGORY + " = ?";
+        String[] selectionArgs = {"1"};
+        Cursor c = db.query(TodosContract.TodosEntry.TABLE_NAME,
+                projection, selection, selectionArgs, null, null, null);
+
+        int i = c.getCount();
+        Log.d("Record count", String.valueOf(i));
     }
 }
